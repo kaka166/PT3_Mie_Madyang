89c9dcaa6873f059c7a0fc1a13d865143718e169
Farelzy-Rz <73918830+farelzy@users.noreply.github.com>
2026-04-15 08:50:57 +0700

    Clear allowed_origins_patterns in CORS config
    Remove specific allowed origins pattern for CORS.

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -9,9 +9,7 @@ return [
 
     'allowed_origins' => [],
 
-    'allowed_origins_patterns' => [
-        '#^https:\/\/.*\.vercel\.app$#'
-    ],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
