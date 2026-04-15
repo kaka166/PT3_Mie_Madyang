256e69a56f124ad8c1de4ee4600eee19f1971afd
Farelzy-Rz <73918830+farelzy@users.noreply.github.com>
2026-04-15 08:50:33 +0700

    clear cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -7,12 +7,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://mie-madyang.farelzy.my.id',
-        'https://pt-3-mie-madyang-gamma.vercel.app', 
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-    ],
+    'allowed_origins' => [],
 
     'allowed_origins_patterns' => [
         '#^https:\/\/.*\.vercel\.app$#'
