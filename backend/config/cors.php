c90a814f91a626aaa848339d34e0adf1c2e9e7db
kaka166 <hafizhfaruq858@gmail.com>
2026-04-18 16:10:51 +0700

    fix corss

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
     'allowed_origins' => [
         'http://localhost:3000',
